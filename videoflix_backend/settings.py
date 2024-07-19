471f5bdfee0164317c7341da15f70f428dfa82db
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-07-19 10:38:48 +0200

    allow frontend to login

--- videoflix_backend/settings.py
+++ videoflix_backend/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200',]
+
+
+
 
 # Application definition
 
@@ -48,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
