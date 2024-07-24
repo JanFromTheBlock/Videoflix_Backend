71330bc8e7a54f8a1700a15e0152ae5b62b87621
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-07-24 16:51:30 +0200

    error handling part1

--- videoflix/views.py
+++ videoflix/views.py
@@ -5,11 +5,11 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import status
 from django.core.mail import EmailMessage, send_mail
-from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
+from django.contrib import messages
 from videoflix_backend import settings
 from django.http import HttpResponseRedirect
 from .tokens import generate_token
@@ -31,36 +31,51 @@ class LoginView(ObtainAuthToken):
         
 class RegisterView(APIView):
     def post(self, request, *args, **kwargs):
-        myuser = User.objects.create_user(username = request.data.get("username"), 
-                                        email= request.data.get("email"),
+        username = request.data.get("username")
+        email= request.data.get("email")
+        
+        user_exists = User.objects.filter(username=username).exists()
+        email_exists = User.objects.filter(email=email).exists()
+        
+        if user_exists or email_exists:
+            errors = {}
+            if user_exists:
+                errors['username'] = "Username ist bereits vergeben"
+            if email_exists:
+                errors['email'] = "E-Mail ist bereits vergeben" 
+            return Response({'errors': errors})  
+        else:
+            myuser = User.objects.create_user(
+                                        username = username, 
+                                        email= email,
                                         password=request.data.get("password"))
-        myuser.is_active = False
-        myuser.save()
+            myuser.is_active = False
+            myuser.save()
         
-        subject = "Welcome to Our Django User Registration System"
-        message = f"Hello !\n\nThank you for registering on our website. Please confirm your email address to activate your account.\n\nRegards,\nThe Django Team"
-        from_email = settings.EMAIL_HOST_USER
-        to_list = [myuser.email]
-        send_mail(subject, message, from_email, to_list, fail_silently=False)
+            subject = "Welcome to Our Django User Registration System"
+            message = f"Hello !\n\nThank you for registering on our website. Please confirm your email address to activate your account.\n\nRegards,\nThe Django Team"
+            from_email = settings.EMAIL_HOST_USER
+            to_list = [myuser.email]
+            send_mail(subject, message, from_email, to_list, fail_silently=False)
         
-        current_site = get_current_site(request)
-        email_subject = "Confirm Your Email Address"
-        message2 = render_to_string('email_confirmation.html', {
-        'domain': current_site.domain,
-        'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-        'token': generate_token.make_token(myuser)
-        })
+            current_site = get_current_site(request)
+            email_subject = "Confirm Your Email Address"
+            message2 = render_to_string('email_confirmation.html', {
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+            })
       
-        send_mail(email_subject, message2, from_email, to_list, fail_silently=False)
-        messages.success(request, "Your account has been created successfully! Please check your email to confirm your email address and activate your account.")
+            send_mail(email_subject, message2, from_email, to_list, fail_silently=False)
+            messages.success(request, "Your account has been created successfully! Please check your email to confirm your email address and activate your account.")
         
         
-        return Response(
-            {
-            'user_id': myuser.pk,
-            'email': myuser.email
-            }
-        )
+            return Response(
+                {
+                'user_id': myuser.pk,
+                'email': myuser.email
+                }
+            )
         
 def activate(request, uidb64, token):
         try:
