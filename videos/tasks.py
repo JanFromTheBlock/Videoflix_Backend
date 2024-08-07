899c98b3e4c96cdfbbc74b962653033c7bfc899a
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-08-07 09:24:15 +0200

    convert to 3 resolutions

--- videos/tasks.py
+++ videos/tasks.py
@@ -1,12 +1,15 @@
 import subprocess
 import os
 
-def convert_480p(source):
-    target = source.split('.')[0] + '_480p.mp4'
+def convert(source):
     ffmpeg_path = r'C:\usr\ffmpeg\bin\ffmpeg.exe'
-    cmd = '{} -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(ffmpeg_path, source, target)
-    print(f'Executing command: {cmd}')
-    subprocess.run(cmd, check=True, shell=True, text=True)
+    resolutions = ['480', '720', '1080']
+    
+    for resolution in resolutions:
+        target = source.split('.')[0] + f'_{resolution}p.mp4'
+        cmd = '{} -i "{}" -s hd{} -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(ffmpeg_path, source, resolution, target)
+        print(f'Executing command: {cmd}')
+        subprocess.run(cmd, check=True, shell=True, text=True)
     
 def video_upload_to(instance, filename):
     name, ext = os.path.splitext(filename)
