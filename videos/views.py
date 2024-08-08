2d04cc8f06211a89702a1c436333e5fedb4b8ca8
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-08-08 09:25:38 +0200

    set authentification fpr videoModel

--- videos/views.py
+++ videos/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework.authtoken.views import APIView, Response
 from videos.models import Video
 from .serializers import VideoSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class VideoView(APIView):
-    #authentication_classes = [authentication.TokenAuthentikation]
-    permission_classes = []
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     
     def get(self, request, format=None):
         videos = Video.objects.all()
