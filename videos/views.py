3fe954d2a7f8ddb5c269a769224c76905bf2b209
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-08-13 09:29:07 +0200

    catch error when wrong video id

--- videos/views.py
+++ videos/views.py
@@ -4,6 +4,7 @@ from videos.models import Video
 from .serializers import VideoSerializer, SingleVideoSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 class VideoView(APIView):
     authentication_classes = [TokenAuthentication]
@@ -18,7 +19,14 @@ class SingleVideoView(APIView):
     
     def get(self, request, format=None):
         videoId = request.query_params.get("video_id")  # Hier ändern
-        singleVideo = Video.objects.get(pk=videoId)
-        serializer = SingleVideoSerializer(singleVideo)
-        return Response(serializer.data)
+        id_exists = Video.objects.filter(pk=videoId).exists()
+        if id_exists:
+            singleVideo = Video.objects.get(pk=videoId)
+            serializer = SingleVideoSerializer(singleVideo)
+            return Response(serializer.data)
+        else:
+            errors = {}
+            errors['username'] = "das gewünschte Video ist aktuell nicht verfügbar"
+            return Response({'errors': errors}, status=status.HTTP_400_BAD_REQUEST) 
+            
         
